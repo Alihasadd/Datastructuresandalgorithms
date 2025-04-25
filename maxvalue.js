f01f565be99cd5182571de0c6845cdff7a5d44ee
Ali Vidler <alisonvidler@gmail.com>
2025-04-25 01:34:58 -0400

    Solved using spread operator and math.max

---
+++ maxvalue.js
@@ -0,0 +1,13 @@
+//Write a function, maxValue, that takes in array of numbers as an argument. The function should return the largest number in the array.
+
+
+const maxValue = (nums) => {
+  
+    return Math.max(...nums)
+  };
+  
+  module.exports = {
+    maxValue,
+  };
+  
+  
