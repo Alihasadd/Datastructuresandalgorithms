84db9b6a81a4caf7c62de811362657adfaaadd7f
Ali Vidler <alisonvidler@gmail.com>
2025-04-26 14:31:48 -0400

    Solved fizz buzz using the push method for loop and if statements

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,19 @@
 //
 
 const fizzBuzz = (n) => {
-    // todo
+    let arr = []
+    for (let i = 1; i <= n; i++){
+      if ( i % 3 === 0 && i % 5 === 0){
+      arr.push('fizzbuzz');
+    } else if (i % 3 === 0){
+      arr.push('fizz');
+    } else if (i % 5 === 0){
+      arr.push('buzz');
+      } else {
+      arr.push(i);
+      }
+    }
+    return arr
   };
   
   module.exports = {
