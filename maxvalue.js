83881885168e822fbd546853e5419968461e446a
Ali Vidler <alisonvidler@gmail.com>
2025-04-27 21:32:56 -0400

    Solved using the loop and the > and infinity

--- maxvalue.js
+++ maxvalue.js
@@ -10,4 +10,26 @@ const maxValue = (nums) => {
     maxValue,
   };
   
+
+  // or
+
+  const maxValue = (nums) => {
+    let max = -Infinity
+    console.log(nums)
+    for (let i = 0; i <= nums.length; i++){
+      if (nums[i] >= max){
+        max = nums[i]
+      }
+    }
+    return max
+  };
+  
+  maxValue([4, 7, 2, 8, 10, 9]); // -> 10
+  
+  
+  module.exports = {
+    maxValue,
+  };
+  
+  
   
